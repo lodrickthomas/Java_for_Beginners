bb9275c19680aa349759c3e5974c332d7d8c418e
XannyB <xannyb@gmail.com>
2014-09-23 18:40:24 +0100

    Initial Commit

---
+++ Variables.java
@@ -0,0 +1,19 @@
+public class Variables
+{
+	public static void main(String[] args)
+	{
+		int number =  100;
+		double rate = 2.34;
+		String firstName;
+		char letter;
+		boolean flag;
+
+		int salary, hoursWorked;
+		String middleName, lastName, initials;
+		// extra comment
+		firstName = "Simon"; // assignment statements
+		number = 20 * 5;
+		letter = 'a';
+		flag = true;
+	}
+}
