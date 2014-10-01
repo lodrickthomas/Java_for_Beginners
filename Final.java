23a616b2eedb9c0de2bf23ebde6e6e3d418f1656
XannyB <xannyb@gmail.com>
2014-10-01 22:18:33 +0100

    Add Operator, OrderOps and Final Examples.
    Added Operator, OrderOps and Final Examples from start of chapter.

---
+++ Final.java
@@ -0,0 +1,10 @@
+public class Final //constant
+{
+	public static void main(String[] args)
+	{
+		final double PI = 3.14159; //CAPITALS are good for final/constants
+		double radius = 2.0;
+		double area = PI * (radius * radius);
+		final String GREETING = "Hello";
+	}
+}
---
+++ OrderOps.java
@@ -0,0 +1,12 @@
+public class OrderOps
+{
+	public static void main(String[] args)
+	{
+		int number = (3 + 4) * 5;
+		System.out.println(number);
+		double number2 = 10 - 2.0 / 14.0 + 3;
+		System.out.println(number2);
+		double number3 = (10 - 2.0) / (14.0 + 3);
+		System.out.println(number3);
+	}
+}
